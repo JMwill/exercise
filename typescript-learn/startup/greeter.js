fa04c70cb1df09b686076a564f6f55d42f9b0019
JMwill <787505998@qq.com>
2017-07-21 11:17:37 +0800

    开始使用

---
+++ typescript-learn/startup/greeter.js
@@ -0,0 +1,6 @@
+function greeter(person) {
+    return 'Hello' + person;
+}
+// var user = 'Jane User';
+var user = [0, 1, 'string'];
+document.body.innerHTML = greeter(user);
---
+++ typescript-learn/startup/greeter.ts
@@ -0,0 +1,8 @@
+function greeter(person: string) {
+    return 'Hello' + person;
+}
+
+var user = 'Jane User';
+
+document.body.innerHTML = greeter(user);
+

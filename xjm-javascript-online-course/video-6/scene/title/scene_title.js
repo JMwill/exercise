caa730041c7b622dbdb24c7379390a84e18a382e
Will <lijiaming@ifanr.com>
2017-10-06 00:16:42 +0800

    完成基本功能

--- xjm-javascript-online-course/video-6/scene/title/scene_title.js
+++ xjm-javascript-online-course/video-6/scene/title/scene_title.js
@@ -1,18 +1,14 @@
-var SceneTitle = function (game) {
-    var s = {
-        game: game,
+class SceneTitle extends WillScene {
+    constructor(game) {
+        super(game)
+
+        game.registerAction('k', function (e) {
+            var s = Scene(game)
+            game.replaceScene(s)
+        })
     }
 
-    game.registerAction('k', function (e) {
-        var s = Scene(game)
-        game.replaceScene(s)
-    })
-
-    s.draw = function() {
-        game.context.fillText('按 k 开始游戏', 100, 190)
+    draw() {
+        this.game.context.fillText('按 k 开始游戏', 100, 190)
     }
-
-    s.update = function() {}
-
-    return s
 }
